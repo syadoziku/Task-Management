1f44b08dd533c8f71df3aec0cc9de7be45a7e5e3
syadoziku <syadoziku@icloud.com>
2019-09-04 13:22:05 +0000

    9/4フォーームの無効化

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,3 +2,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
 end
+
+
+class UserController < ApplicationController
+  before_action :authenticate_user, only: [:index, :show, :edit, :update]
+  before_action :limitation_login_user, only: [:new, :create, :login_page, :login]
+end
