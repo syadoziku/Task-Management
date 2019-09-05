d4aad3251478cb127abbf89633021d4c2fad6ed6
syadoziku <syadoziku@icloud.com>
2019-09-05 05:00:51 +0000

    9/5管理者権限追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,4 +3,10 @@
 User.create!( name: "管理者",
               email: "sample@email.com",
               password: "password",
-              password_confirmation: "password")
+              password_confirmation: "password",
+              admin: true)
+              
+User.create!( name: "ゲスト",
+              email: "guest@email.com",
+              password: "password",
+              password_confirmation: "password",)
