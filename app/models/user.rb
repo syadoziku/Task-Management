a91674840e4e644f4079116dbf4dd6d6e73ab345
syadoziku <syadoziku@icloud.com>
2019-09-06 09:49:24 +0000

    department2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,25 @@
 class User < ApplicationRecord
+  # 仮装属性
   attr_accessor :remember_token
   before_save { self.email = email.downcase }
+  
+  validates :name, presence: true, length: { maximum: 50 }
+  
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :name,  presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 100 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: true
+  validates :department, length:  {  in: 2..50 }, allow_blank: true
+  
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
   
+  
+  
   def User.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
-      
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : 
+    BCrypt::Engine.costBCrypt::Password.create(string, cost: cost)
   end
   
   def User.new_token
