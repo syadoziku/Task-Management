8f499967a5c3cae54664664c84f5bcc52c0c27b9
syadoziku <syadoziku@icloud.com>
2019-09-10 12:31:14 +0000

    posts index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  
+  get 'posts/index', to: 'posts#index'
   get 'tasks/top', to: 'tasks#top'
 
   root 'static_pages#top'
@@ -10,4 +12,5 @@ Rails.application.routes.draw do
   
   
   resources :users
+  resources :tasks
 end
